587cc8da8575c1aee8db6c0e7db8235269cce58d
cute3954 <cute3954@naver.com>
2020-04-21 16:41:37 +0900

    4/21 GoFデザインパターン_Decoratorパターン学習

---
+++ DesignPattern/src/decoratorPattern_class/Base.java
@@ -0,0 +1,10 @@
+package decoratorPattern_class;
+
+public class Base implements Beverage {
+
+	@Override
+	public int getTotalPrice() {
+		return 200;
+	}
+
+}
---
+++ DesignPattern/src/decoratorPattern_class/Beverage.java
@@ -0,0 +1,5 @@
+package decoratorPattern_class;
+
+public abstract interface Beverage {
+	public abstract int getTotalPrice();
+}

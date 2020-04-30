5b779db4ad3a45c46776c68c9fabf2bad5633809
cute3954 <cute3954@naver.com>
2020-04-30 15:07:26 +0900

    4/30 GoFデザインパターン_Stateパターン学習

---
+++ DesignPattern/src/statePattern_state/State.java
@@ -0,0 +1,15 @@
+package statePattern_state;
+
+import statePattern_context.Context;
+
+// 金庫の状態を表すインターフェース
+public interface State {
+	// 時刻設定
+	public abstract void doClock(Context context, int hour);
+	// 金庫使用
+	public abstract void doUse(Context context);
+	// 非常ベル
+	public abstract void doAlarm(Context context);
+	// 通常通話
+	public abstract void doPhone(Context context);
+}
